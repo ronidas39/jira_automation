1882e16c2130e67c1903a85dd96f095b7762748d
MagdelineMabusela <113175636+MagdelineMabusela@users.noreply.github.com>
2023-03-24 15:19:43 +0200

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -1,6 +1,6 @@
 import requests
 import json
-url="https://totaltechnology.atlassian.net//rest/api/2/issue"
+url="https://magdeline22.atlassian.net//rest/api/2/issue"
 headers={
     "Accept": "application/json",
     "Content-Type": "application/json"
@@ -10,9 +10,9 @@ payload=json.dumps(
     "fields": {
        "project":
        {
-          "key": "TTS"
+          "key": "TJJ"
        },
-       "summary": "created for test",
+       "summary": "creating for test",
        "description": "Created for test11",
        "issuetype": {
           "name": "Task"
@@ -20,9 +20,10 @@ payload=json.dumps(
    }
 }
 )
-response=requests.post(url,headers=headers,data=payload,auth=("ronidas071@gmail.com","wg5OpdxcSbGvlykzUhm5C939"))
-data=response.json()
-print(data["id"])
+response=requests.post(url,headers=headers,data=payload,
+auth=("magdamabusela@gmail.com",
+"ATATT3xFfGF0UzMaW1lfogT_YlLrM820oSsukcmfHPwZ0WRsK3wN2OmSed4h9zZv_NITHjePpZ-cm4KUK8n0HdsZCQoIUscYjbNpOwj0Inwh0GOy6tq2y8eReZTvdKxhTgmtUzEaT1PxOHbLI6Cn99qyTg0wkYMDkUMfm2dIfWRMx4ftOZFDFEQ=CA706B00"))
+print(response.text)
 
 
 
